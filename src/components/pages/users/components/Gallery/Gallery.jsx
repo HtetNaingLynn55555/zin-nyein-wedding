6af2ae8993f1362414e08d339f7b89be096ab50c
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-21 18:35:18 +0700

    add image gallery responsive

--- src/components/pages/users/components/Gallery/Gallery.jsx
+++ src/components/pages/users/components/Gallery/Gallery.jsx
@@ -6,6 +6,9 @@ export default function Gallery() {
       <div className="col-span-12 md:col-start-3 md:col-end-11">
         <GalleryHeader />
       </div>
+      <div className=" col-span-12 md:col-start-2 md:col-end-12 ">
+        <GalleryPhoto />
+      </div>
     </div>
   );
 }
--- src/components/pages/users/components/Gallery/GalleryPhoto.jsx
+++ src/components/pages/users/components/Gallery/GalleryPhoto.jsx
@@ -1,6 +1,73 @@
-export default function GalleryPhoto()
-{
-    return(<div>
-        Gallery Photo
-    </div>)
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { RowsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/rows.css";
+import { useState } from "react";
+export default function GalleryPhoto() {
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(-1);
+  return (
+    <div>
+      {/* <RowsPhotoAlbum
+        photos={[
+          { src: "/images/coupleOne.jpg", width: 800, height: 600 },
+          { src: "/images/coupleTwo.jpg", width: 800, height: 600 },
+          { src: "/images/coupleThree.jpg", width: 800, height: 600 },
+          { src: "/images/coupleOne.jpg", width: 800, height: 600 },
+          { src: "/images/coupleTwo.jpg", width: 800, height: 600 },
+          { src: "/images/coupleThree.jpg", width: 800, height: 600 },
+          { src: "/images/coupleOne.jpg", width: 800, height: 600 },
+          { src: "/images/coupleTwo.jpg", width: 800, height: 600 },
+          { src: "/images/coupleThree.jpg", width: 800, height: 600 },
+        ]}
+      /> */}
+      <RowsPhotoAlbum
+        photos={[
+          { src: "/images/coupleOne.jpg", width: 800, height: 600 },
+          { src: "/images/coupleTwo.jpg", width: 800, height: 600 },
+          { src: "/images/coupleThree.jpg", width: 800, height: 600 },
+          { src: "/images/coupleOne.jpg", width: 800, height: 600 },
+          { src: "/images/coupleTwo.jpg", width: 800, height: 600 },
+          { src: "/images/coupleThree.jpg", width: 800, height: 600 },
+          { src: "/images/coupleOne.jpg", width: 800, height: 600 },
+          { src: "/images/coupleTwo.jpg", width: 800, height: 600 },
+          { src: "/images/coupleThree.jpg", width: 800, height: 600 },
+        ]}
+        targetRowHeight={150}
+        onClick={({ index: current }) => setIndex(current)}
+      />
+
+      <Lightbox
+        index={index}
+        slides={[
+          { src: "/images/coupleOne.jpg" },
+          { src: "/images/coupleTwo.jpg" },
+          { src: "/images/coupleThree.jpg" },
+          { src: "/images/coupleOne.jpg" },
+          { src: "/images/coupleTwo.jpg" },
+          { src: "/images/coupleThree.jpg" },
+          { src: "/images/coupleOne.jpg" },
+          { src: "/images/coupleTwo.jpg" },
+          { src: "/images/coupleThree.jpg" },
+        ]}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+      />
+
+      {/* <button type="button" onClick={() => setOpen(true)}>
+        Open Lightbox
+      </button>
+
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={[
+          { src: "/images/coupleOne.jpg" },
+          { src: "/images/coupleTwo.jpg" },
+          { src: "/images/coupleThree.jpg" },
+        ]}
+      /> */}
+    </div>
+  );
 }
