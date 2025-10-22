990c8e3a77191a4a3a9652af75300c36c5836e5a
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-23 01:48:01 +0700

    add 404 not found style

--- src/components/pages/users/UserNotFound.jsx
+++ src/components/pages/users/UserNotFound.jsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Fallback = () => {
-  return <div>User Not Found</div>;
+  return (
+    <main className="grid min-h-full max-h-screen place-items-center bg-white  px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className=" font-secondary text-5xl font-semibold text-gray-900">
+          404
+        </p>
+        <h1 className="mt-4 text-5xl font-medium  font-primary tracking-tight text-balance text-gray-900 sm:text-7xl">
+          Page not found
+        </h1>
+        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"></p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            to={"/"}
+            href="#"
+            className="rounded-md font-secondary bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-teal-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Home
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Fallback;
