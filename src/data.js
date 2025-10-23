5a5e8bf568d27d4baa43b530945f6d5c71bb6f18
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-23 21:48:20 +0700

    change extension

--- src/data.js
+++ src/data.js
@@ -20,85 +20,83 @@ let cover = [
 const gallery = [
   {
     key: 1,
-    src: "/images/gallery/gallery1.jpg",
+    src: "/images/gallery/gallery1.webp",
   },
   {
     key: 2,
-    src: "/images/gallery/gallery2.jpg",
+    src: "/images/gallery/gallery2.webp",
   },
   {
     key: 3,
-    src: "/images/gallery/gallery3.jpg",
+    src: "/images/gallery/gallery3.webp",
   },
   {
     key: 4,
-    src: "/images/gallery/gallery4.jpg",
+    src: "/images/gallery/gallery4.webp",
   },
   // {
   //   key: 5,
-  //   src: "/images/gallery/gallery5.jpg",
+  //   src: "/images/gallery/gallery5.webp",
   // },
   {
     key: 6,
-    src: "/images/gallery/gallery6.jpg",
+    src: "/images/gallery/gallery6.webp",
   },
   {
     key: 7,
-    src: "/images/gallery/gallery7.jpg",
+    src: "/images/gallery/gallery7.webp",
   },
   // {
   //   key: 8,
-  //   src: "/images/gallery/gallery8.jpg",
+  //   src: "/images/gallery/gallery8.webp",
   // },
   {
     key: 9,
-    src: "/images/gallery/gallery9.jpg",
+    src: "/images/gallery/gallery9.webp",
   },
 
   {
     key: 10,
-    src: "/images/gallery/gallery10.jpg",
+    src: "/images/gallery/gallery10.webp",
   },
   // {
   //   key: 11,
-  //   src: "/images/gallery/gallery11.jpg",
+  //   src: "/images/gallery/gallery11.webp",
   // },
 
   {
     key: 12,
-    src: "/images/gallery/gallery12.jpg",
+    src: "/images/gallery/gallery12.webp",
   },
 
   {
     key: 13,
-    src: "/images/gallery/gallery13.jpg",
+    src: "/images/gallery/gallery13.webp",
   },
   {
     key: 14,
-    src: "/images/gallery/gallery14.jpg",
+    src: "/images/gallery/gallery14.webp",
   },
   {
     key: 15,
-    src: "/images/gallery/gallery15.jpg",
+    src: "/images/gallery/gallery15.webp",
   },
   {
     key: 16,
-    src: "/images/gallery/gallery16.jpg",
+    src: "/images/gallery/gallery16.webp",
   },
   {
     key: 17,
-    src: "/images/gallery/gallery17.jpg",
+    src: "/images/gallery/gallery17.webp",
   },
   {
     key: 18,
-    src: "/images/gallery/gallery18.jpg",
+    src: "/images/gallery/gallery18.webp",
   },
   {
     key: 19,
-    src: "/images/gallery/gallery19.jpg",
+    src: "/images/gallery/gallery19.webp",
   },
-
-  
 ];
 const invitionPhoto = {
   src: "/images/invition/invition.jpg",
