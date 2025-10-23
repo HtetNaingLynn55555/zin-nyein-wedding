67189eeb4d6f86b12182609b44b0f0d70df94e51
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-23 21:52:19 +0700

    change count down second to appear in mobile

--- src/components/pages/users/components/Countdown.jsx
+++ src/components/pages/users/components/Countdown.jsx
@@ -1,6 +1,6 @@
 export default function Countdown({ time }) {
   return (
-    <div className=" font-primary text-2xl flex justify-center items-center rounded-full w-15 h-15 font-bold border-2  border-[#E6A817]">
+    <div className=" font-primary text-[16px] md:text-2xl flex justify-center items-center rounded-full w-10 h-10 md:w-10 md:h-10 font-bold border md:border-2  border-[#E6A817]">
       {time}
     </div>
   );
