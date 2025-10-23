b3cf35f6ef2654530c845ac943f246079a583e2f
HtetNaingLynn55555 <htetnainglynnhnl@gmail.com>
2025-10-23 19:10:21 +0700

    chage photos in gallery

--- src/data.js
+++ src/data.js
@@ -19,25 +19,50 @@ let cover = [
 
 const gallery = [
   {
+    key: 1,
+    src: "/images/gallery/gallery1.jpg",
+  },
+  {
+    key: 2,
+    src: "/images/gallery/gallery2.jpg",
+  },
+  {
+    key: 3,
+    src: "/images/gallery/gallery3.jpg",
+  },
+  {
     key: 4,
     src: "/images/gallery/gallery4.jpg",
   },
-  {
-    key: 5,
-    src: "/images/gallery/gallery5.jpg",
-  },
+  // {
+  //   key: 5,
+  //   src: "/images/gallery/gallery5.jpg",
+  // },
   {
     key: 6,
     src: "/images/gallery/gallery6.jpg",
   },
   {
-    key: 8,
-    src: "/images/gallery/gallery8.jpg",
-  },
-  {
     key: 7,
     src: "/images/gallery/gallery7.jpg",
   },
+  // {
+  //   key: 8,
+  //   src: "/images/gallery/gallery8.jpg",
+  // },
+  {
+    key: 9,
+    src: "/images/gallery/gallery9.jpg",
+  },
+
+  {
+    key: 10,
+    src: "/images/gallery/gallery10.jpg",
+  },
+  // {
+  //   key: 11,
+  //   src: "/images/gallery/gallery11.jpg",
+  // },
 
   {
     key: 12,
@@ -45,31 +70,35 @@ const gallery = [
   },
 
   {
-    key: 10,
-    src: "/images/gallery/gallery10.jpg",
-  },
-  {
-    key: 1,
-    src: "/images/gallery/gallery1.jpg",
-  },
-  {
-    key: 11,
-    src: "/images/gallery/gallery11.jpg",
-  },
-
-  {
-    key: 9,
-    src: "/images/gallery/gallery9.jpg",
+    key: 13,
+    src: "/images/gallery/gallery13.jpg",
   },
   {
     key: 14,
     src: "/images/gallery/gallery14.jpg",
   },
-
   {
-    key: 20,
-    src: "/images/gallery/gallery20.jpg",
+    key: 15,
+    src: "/images/gallery/gallery15.jpg",
   },
+  {
+    key: 16,
+    src: "/images/gallery/gallery16.jpg",
+  },
+  {
+    key: 17,
+    src: "/images/gallery/gallery17.jpg",
+  },
+  {
+    key: 18,
+    src: "/images/gallery/gallery18.jpg",
+  },
+  {
+    key: 19,
+    src: "/images/gallery/gallery19.jpg",
+  },
+
+  
 ];
 const invitionPhoto = {
   src: "/images/invition/invition.jpg",
